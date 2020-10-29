3a77d0f1c92d88e5a7abbb08b98e7842819ff5fe
Vanavan <chzhank@gmail.com>
2020-10-29 13:35:56 +0200

    HW2

---
+++ Homewrok/SecretString/SecretString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SecretString.Check;
+
+namespace SecretString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SecString str = new SecString();
+            Console.WriteLine("Enter secret string: ");
+            str.Line = Console.ReadLine();
+            Console.WriteLine("Enter password: ");
+            string password = Console.ReadLine();
+            
+            str.CheckPass(password);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homewrok/SecretString/SecretString/SecString.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretString.Check
+{
+    class SecString
+    {
+        public string password = "12345";
+        public string Line 
+        {
+            get;
+            set;  
+        }
+
+        public void CheckPass(string pass)
+        {
+            if (pass == password)
+            {
+                Console.WriteLine("Password correct your line: " + Line);
+                Console.WriteLine("What do you want Change Password or Secreat String(choose 1 or 2): ");
+                string ch = Console.ReadLine();
+                switch (ch) 
+                {
+                    case "1":
+                        PasswordChange(pass);
+                        break;
+                    case "2":
+                        StringChange(pass);
+                        break;
+                    default: Console.WriteLine("Wrong case!!!");
+                        break;
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Encorrect password!");
+            }
+
+        }
+
+        public void PasswordChange(string pass) 
+        {
+            if (pass == password)
+            {
+                Console.WriteLine("New password");
+                password = Console.ReadLine();
+                Console.WriteLine("Your new password: " + password);
+            }
+            else 
+            {
+                Console.WriteLine("Encorrect password");
+            }
+        }
+
+        public void StringChange(string pass) 
+        {
+            if (pass == password) 
+            {
+                Console.WriteLine("Write new string: ");
+                Line = Console.ReadLine();
+                Console.WriteLine("Your new Secret string: " + Line);
+                
+            }
+            else 
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
