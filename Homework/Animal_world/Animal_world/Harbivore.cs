f1e9e8cb9c0e56d56a211b11c733a896088ef78f
Vanavan <chzhank@gmail.com>
2020-11-05 13:44:36 +0200

    HW3

---
+++ Homework/Animal_world/Animal_world/Harbivore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal_world
+{
+    class Harbivore : Animal
+    {
+        public double Defense { get; set; }
+        public Harbivore() : base()
+        {
+        }
+        public Harbivore(string name, double defense) : base()
+        {
+            this.Defense = defense;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"Name={Name}: Species={Species}: Satiety={Satiety}: HP={HitPoints}: Defense={Defense}");
+        }
+    }
+}
---
+++ Homework/Animal_world/Animal_world/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Animal_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Harbivore h = new Harbivore();
+            h.Name = "Rabbit";
+            h.Species = "White";
+            h.HitPoints = 100;
+            h.Satiety = 100;
+            h.Defense = 20;
+            h.Display();
+
+
+            Predator p = new Predator();
+            p.Name = "Lion";
+            p.Species = "Big";
+            p.HitPoints = 70;
+            p.Satiety = 60;
+            p.Attack = 80;
+            p.Display();
+
+        }
+    }
+}
