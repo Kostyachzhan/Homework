e2dbf481eed89c5edd18c6d0cf672ac8d331b0f3
Vanavan <chzhank@gmail.com>
2020-11-13 08:09:38 +0200

    l

---
+++ Homework/Secret String/Secret String/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Secret_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter password: ");
+            string password = Console.ReadLine();
+            Console.WriteLine("Enter secret string: ");
+            string line = Console.ReadLine();
+            SecString str = new SecString(password, line);
+            Console.WriteLine(str.ReturnString(password));
+            string new_password = Console.ReadLine();
+            string new_line = Console.ReadLine();
+
+            str.PasswordChange(password, new_password);
+            Console.WriteLine(str.ReturnString(password));
+            Console.WriteLine(str.ReturnString(new_password));
+            str.StringChange(new_password, new_line);
+            Console.WriteLine(str.ReturnString(new_password));
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Homework/Secret String/Secret String/SecString.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Secret_String
+{
+    class SecString
+    {
+        private string password;
+        private string line;
+
+
+        public SecString(string password, string line)
+        {
+            this.password = password;
+            this.line = line;
+        }
+
+        public void PasswordChange(string pass, string new_pass)
+        {
+            if (pass == password)
+            {
+                password = new_pass;
+            }
+            else
+            {
+
+                throw new Exception("Incorrect password");
+            }
+
+        }
+        public void StringChange(string pass, string new_string)
+        {
+            if (pass == password)
+            {
+                line = new_string;
+            }
+            else
+            {
+                throw new Exception("Incorrect password");
+            }
+        }
+
+        public string ReturnString(string pass)
+        {
+            if (pass == password)
+            {
+                return line;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
+
